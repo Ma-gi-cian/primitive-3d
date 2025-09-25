153ed8a45691806139a18afdb0295edf8fcd55e5
Aditya Jha <aditya01jha.02@gmail.com>
2025-09-25 17:13:04 +0000

    feat: camera is here now

--- src/types.h
+++ src/types.h
@@ -1,12 +1,29 @@
 #pragma once
 
 typedef struct {
-	int x; 
-	int y;
-	int z;
+	float x; 
+	float y;
+	float z;
 } vec3;
+
+typedef struct {
+	float x;
+	float y;
+} vec2;
 
 typedef struct {
 	int x;
 	int y;
-} vec2;
+	int z;
+} ivec3;
+
+typedef struct {
+	int x;
+	int y;
+} ivec2;
+
+typedef struct {
+	vec3 position;
+	vec3 rotation;
+	float fov;
+} Camera;
